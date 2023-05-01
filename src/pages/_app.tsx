792a0b80dee213e024719a53b6799cb3cdb79f2e
JoyCoffeeAddict <joy.rzepecka@gmail.com>
2023-05-01 17:01:30 +0200

    style: basic styling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,24 @@
-import { type AppType } from "next/app";
+import { type AppType } from "next/app"
 
-import { api } from "~/utils/api";
+import { api } from "~/utils/api"
 
-import "~/styles/globals.css";
+import "~/styles/globals.css"
+import Head from "next/head"
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+  return (
+    <>
+      <Head>
+        <title>List manager</title>
+        <meta
+          name="description"
+          content="Basic lsit manager for easily sharing content"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
 
-export default api.withTRPC(MyApp);
+export default api.withTRPC(MyApp)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,15 @@
-import { type NextPage } from "next";
-import Head from "next/head";
+import { type NextPage } from "next"
 
 const Home: NextPage = () => {
   return (
     <>
-      <Head>
-        <title></title>
-        <meta name="description" content="" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#b00b69] to-[#420690]"></main>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#b00b69] to-[#420690] text-white">
+        <div className="overflow-x-hidden overflow-y-scroll border-2 border-blue-400 p-4 md:max-w-2xl">
+          lorem ipsum
+        </div>
+      </main>
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
